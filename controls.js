baba803e3afd877e8670da942a276423bc18b950
Wendy <wendingoli@gmail.com>
2023-10-27 17:55:24 -0700

    Adding RHS ui elements as well as adding styling changes for element colors. Start button also includes a wiggle animation on hover

--- controls.js
+++ controls.js
@@ -1,42 +1,33 @@
+function createSelectionControl(id, title) {
+    const selectControl = document.createElement('div');
+    selectControl.id = "figure-drawing-extension-select-control-" + id;
+    selectControl.innerHTML = `
+        <h1>${title}</h1>
+        <span class="figure-drawing-extension-select">
+            <button class='figure-drawing-extension-select-button'>-</button>
+            <div class='figure-drawing-extension-selection-level-bar'></div>
+            <button class='figure-drawing-extension-select-button'>+</button>
+        </span>
+    `;
 
-// Create a new div element
+    selectionControls.appendChild(selectControl);
+}
+
+function createMainButton(id, title) {
+    const button = document.createElement('button');
+    button.id = "figure-drawing-extension-main-button-" + id;
+    button.className = "figure-drawing-extension-main-button";
+    button.textContent = title;
+
+    rhsControls.appendChild(button);
+}
+
+/* === Draggable element === */
 const draggableDiv = document.createElement('div');
 draggableDiv.id = "figure-drawing-extension-controls";
 draggableDiv.style.position = 'fixed';
 draggableDiv.style.top = '20px';
 draggableDiv.style.right = '20px';
-
-const timeDisplay = document.createElement('div');
-timeDisplay.id = "figure-drawing-extension-time-display";
-timeDisplay.innerHTML = `
-    <h1>00:00:00</h1>
-`;
-timeDisplay.style.position = 'relative';
-
-const sketchTimeSelect = document.createElement('div');
-sketchTimeSelect.id = "figure-drawing-extension-sketch-time-select"
-sketchTimeSelect.innerHTML = `
-    <h1>sketch time</h1>
-    <span class="figure-drawing-extension-select">
-        <button class='figure-drawing-extension-select-button'>-</button>
-        <div class='figure-drawing-extension-selection-level-bar'></div>
-        <button class='figure-drawing-extension-select-button'>+</button>
-    </span>
-`;
-
-const frameIntervalSelect = document.createElement('div');
-frameIntervalSelect.id = "figure-drawing-extension-frame-interval-select"
-frameIntervalSelect.innerHTML = `
-    <h1>frame interval</h1>
-`;
-
-const selectionControls = document.createElement('div');
-selectionControls.id = "figure-drawing-extension-selection-controls"
-selectionControls.appendChild(sketchTimeSelect);
-selectionControls.appendChild(frameIntervalSelect);
-
-draggableDiv.appendChild(timeDisplay);
-draggableDiv.appendChild(selectionControls);
 
 // Add mousedown event listener to enable dragging
 let isDragging = false;
@@ -62,3 +53,30 @@ document.addEventListener('mouseup', () => {
 
 // Append the draggable div to the body
 document.body.appendChild(draggableDiv);
+
+/* === LHS - Time display === */
+const lhsControls = document.createElement('div');
+lhsControls.id = "figure-drawing-extension-lhs-controls";
+draggableDiv.appendChild(lhsControls);
+
+const timeDisplay = document.createElement('div');
+timeDisplay.id = "figure-drawing-extension-time-display";
+timeDisplay.innerHTML = `
+    <h1>00:00:00</h1>
+`;
+timeDisplay.style.position = 'relative';
+lhsControls.appendChild(timeDisplay);
+
+/* === RHS - Selection controls === */
+const rhsControls = document.createElement('div');
+rhsControls.id = "figure-drawing-extension-rhs-controls";
+draggableDiv.appendChild(rhsControls);
+
+const selectionControls = document.createElement('div');
+selectionControls.id = "figure-drawing-extension-selection-controls";
+rhsControls.appendChild(selectionControls);
+createSelectionControl('sketch-time', 'sketch time');
+createSelectionControl('frame-interval', 'frame interval');
+
+createMainButton("start", "start");
+
