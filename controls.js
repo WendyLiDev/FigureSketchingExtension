4edbd6b89487fe9e9aad5e89fb38633727375361
Wendy <wendingoli@gmail.com>
2023-10-31 15:26:25 -0700

    Refactoring creating buttons and div elements

--- controls.js
+++ controls.js
@@ -1,50 +1,11 @@
+// controls.js
+
 var started = false;
 
-function createSelectionControl(id, title) {
-    const selectControl = document.createElement('div');
-    selectControl.id = "figure-drawing-extension-select-control-" + id;
-    selectControl.innerHTML = `
-        <h1>${title}</h1>
-        <span class="figure-drawing-extension-select">
-            <button class='figure-drawing-extension-select-button'>-</button>
-            <div class='figure-drawing-extension-selection-level-bar'></div>
-            <button class='figure-drawing-extension-select-button'>+</button>
-        </span>
-    `;
-
-    selectionControls.appendChild(selectControl);
-}
-
-function createMainButton(id, title) {
-    const button = document.createElement('button');
-    button.id = "figure-drawing-extension-main-button-" + id;
-    button.className = "figure-drawing-extension-main-button";
-    button.textContent = title;
-    button.addEventListener('mousedown', (e) => {
-        const frameIntervalPreview = document.getElementById('figure-drawing-extension-frame-interval-preview');
-        const themeButton = document.getElementById('figure-drawing-extension-theme-button');
-        const progressBar = document.getElementById('figure-drawing-extension-progress-bar');
-        if(!started) {
-            button.textContent = "end";
-            frameIntervalPreview.hidden = true;
-            themeButton.hidden = true;
-            progressBar.hidden = false;
-            started = true;
-        } else {
-            button.textContent = "start";
-            frameIntervalPreview.hidden = false;
-            themeButton.hidden = false;
-            progressBar.hidden = true;
-            started = false;
-        }
-    });
-
-    rhsControls.appendChild(button);
-}
+/* **************************************************************************  */
 
 /* === Draggable element === */  
-const draggableDiv = document.createElement('div');
-draggableDiv.id = "figure-drawing-extension-controls";
+const draggableDiv = Div("controls");
 draggableDiv.style.position = 'fixed';
 draggableDiv.style.top = '20px';
 draggableDiv.style.right = '20px';
@@ -74,36 +35,31 @@ document.addEventListener('mouseup', () => {
 // Append the draggable div to the body
 document.body.appendChild(draggableDiv);
 
-/* === LHS - Time display === */
-const lhsControls = document.createElement('div');
-lhsControls.id = "figure-drawing-extension-lhs-controls";
+/* ====== LHS - Time display ====== */
+const lhsControls = Div("lhs-controls");
 draggableDiv.appendChild(lhsControls);
 
-const timeDisplay = document.createElement('div');
-timeDisplay.id = "figure-drawing-extension-time-display";
+const timeDisplay = Div("time-display");
 timeDisplay.innerHTML = `
     <h1>00:00:00</h1>
 `;
 timeDisplay.style.position = 'relative';
 lhsControls.appendChild(timeDisplay);
 
-const frameIntervalPreview = document.createElement('div');
-frameIntervalPreview.id = "figure-drawing-extension-frame-interval-preview";
+const frameIntervalPreview = Div("frame-interval-preview");
 frameIntervalPreview.textContent = "3 seconds";
 lhsControls.appendChild(frameIntervalPreview);
 
-const progressBar = document.createElement('div');
-progressBar.id = 'figure-drawing-extension-progress-bar';
+const progressBar = Div('progress-bar');
 progressBar.hidden = true;
 lhsControls.appendChild(progressBar);
 
-const themeButton = document.createElement('button');
-themeButton.id = "figure-drawing-extension-theme-button";
+const themeButton = Button("theme-button");
 themeButton.innerHTML = `
     <img id="figure-drawing-extension-theme-button-icon" alt="An icon for a button that changes the theme for the figure drawing extension">
 `;
 lhsControls.appendChild(themeButton);
-let img = document.getElementById('figure-drawing-extension-theme-button-icon');
+let img = GetElementById('theme-button-icon');
 img.src = chrome.runtime.getURL("./images/theme_icon-dark.png");
 
 // TODO: Store this as a preference
@@ -128,15 +84,76 @@ themeButton.addEventListener('mousedown', () => {
     }
 })
 
-/* === RHS - Selection controls === */
-const rhsControls = document.createElement('div');
-rhsControls.id = "figure-drawing-extension-rhs-controls";
+/* ====== RHS - Selection controls ====== */
+const rhsControls = Div("rhs-controls");
 draggableDiv.appendChild(rhsControls);
 
-const selectionControls = document.createElement('div');
-selectionControls.id = "figure-drawing-extension-selection-controls";
+
+// Selection controls
+const selectionControls = Div("selection-controls");
 rhsControls.appendChild(selectionControls);
 createSelectionControl('sketch-time', 'sketch time');
 createSelectionControl('frame-interval', 'frame interval');
 
+// Main Start & End button
 createMainButton("start", "start");
+
+/* **************************************************************************  */
+/* ***** Create Components *****  */
+function createSelectionControl(id, title) {
+    const selectControl = Div("figure-drawing-extension-select-control-" + id);
+    selectControl.innerHTML = `
+        <h1>${title}</h1>
+        <span class="figure-drawing-extension-select">
+            <button class='figure-drawing-extension-select-button'>-</button>
+            <div class='figure-drawing-extension-selection-level-bar'></div>
+            <button class='figure-drawing-extension-select-button'>+</button>
+        </span>
+    `;
+
+    selectionControls.appendChild(selectControl);
+}
+
+function createMainButton(id, title) {
+    const button = Button("main-button-" + id, title);
+    button.className = "figure-drawing-extension-main-button";
+    button.addEventListener('mousedown', (e) => {
+        const frameIntervalPreview = GetElementById('frame-interval-preview');
+        const themeButton = GetElementById('theme-button');
+        const progressBar = GetElementById('progress-bar');
+        if(!started) {
+            button.textContent = "end";
+            frameIntervalPreview.hidden = true;
+            themeButton.hidden = true;
+            progressBar.hidden = false;
+            started = true;
+        } else {
+            button.textContent = "start";
+            frameIntervalPreview.hidden = false;
+            themeButton.hidden = false;
+            progressBar.hidden = true;
+            started = false;
+        }
+    });
+
+    rhsControls.appendChild(button);
+}
+
+/* **************************************************************************  */
+/* ***** Helpers *****  */
+function Div(id) {
+    const div = document.createElement('div');
+    div.id = "figure-drawing-extension-" + id; 
+    return div;
+}
+
+function Button(id, textContent) {
+    const button = document.createElement('button'); 
+    button.id = "figure-drawing-extension-" + id; 
+    button.textContent = textContent;
+    return button;
+}
+
+function GetElementById(id) {
+    return document.getElementById('figure-drawing-extension-' + id);
+}
